c913848969dc18ebbf48573cab4765c17296dfb0
R <rqou@berkeley.edu>
2022-10-09 15:05:19 -0700

    just inherit from java mod loader

--- src/main/java/nekomods/soft/SoFtLanguageProvider.java
+++ src/main/java/nekomods/soft/SoFtLanguageProvider.java
@@ -1,29 +1,10 @@
 package nekomods.soft;
 
-import net.minecraftforge.forgespi.language.ILifecycleEvent;
-import net.minecraftforge.forgespi.language.IModLanguageProvider;
-import net.minecraftforge.forgespi.language.ModFileScanData;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLanguageProvider;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
-public class SoFtLanguageProvider implements IModLanguageProvider {
-    private static final Logger LOGGER = LogManager.getLogger();
-
+public class SoFtLanguageProvider extends FMLJavaModLanguageProvider {
     @Override
     public String name() {
         return "SoFt";
-    }
-
-    @Override
-    public Consumer<ModFileScanData> getFileVisitor() {
-        return null;
-    }
-
-    @Override
-    public <R extends ILifecycleEvent<R>> void consumeLifecycleEvent(Supplier<R> consumeEvent) {
-        LOGGER.warn("hi hi hi");
     }
 }
