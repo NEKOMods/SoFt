c1387c51effc203d92c5bc1d72aed0be52d10a0c
R <rqou@berkeley.edu>
2023-04-16 17:25:45 +0100

    fix example so all the annotation stuff works now

--- testmod/src/main/scala/com/example/examplemod/ExampleMod.scala
+++ testmod/src/main/scala/com/example/examplemod/ExampleMod.scala
@@ -1,8 +1,33 @@
 package com.example.examplemod
 
+import com.mojang.logging.LogUtils
+import net.minecraft.client.Minecraft
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.server.ServerStartingEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 
-@Mod("examplemod")
+final val MODID = "examplemod"
+val LOGGER = LogUtils.getLogger
+
+@Mod(MODID)
 class ExampleMod {
+  MinecraftForge.EVENT_BUS.register(this)
 
+  @SubscribeEvent
+  def onServerStarting(event: ServerStartingEvent): Unit = {
+    LOGGER.info("HELLO from server starting")
+  }
+}
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Array(Dist.CLIENT))
+class ClientModEvents
+object ClientModEvents {
+  @SubscribeEvent
+  @scala.annotation.static def onClientSetup(event: FMLClientSetupEvent): Unit = {
+    LOGGER.info("HELLO FROM CLIENT SETUP")
+    LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance.getUser.getName)
+  }
 }
